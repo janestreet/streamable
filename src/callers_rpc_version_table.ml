c84288169fcc9867547d617e9470b677f00d0450
public-release <as-public-release@janestreet.com>
2023-11-15 15:50:15 +0000

    v0.17~preview.128.47+23

--- src/callers_rpc_version_table.ml
+++ src/callers_rpc_version_table.ml
@@ -21,18 +21,9 @@ let add_exn (type a) ({ rpc_name; table } : a t) ~version (data : a) =
 let lookup_most_recent t ~callee_menu =
   let open Or_error.Let_syntax in
   let rpc_name = t.rpc_name in
-  let callee_versions = Versioned_rpc.Menu.supported_versions callee_menu ~rpc_name in
   let caller_versions = Rpc_version.Set.of_list (Hashtbl.keys t.table) in
   let%bind version =
-    match Set.max_elt (Set.inter callee_versions caller_versions) with
-    | Some version -> Ok version
-    | None ->
-      Or_error.error_s
-        [%message
-          "caller and callee share no common versions for rpc"
-            (rpc_name : string)
-            (caller_versions : Rpc_version.Set.t)
-            (callee_versions : Rpc_version.Set.t)]
+    Versioned_rpc.Menu.highest_shared_version ~callee_menu ~rpc_name ~caller_versions
   in
   match Hashtbl.find t.table version with
   | Some data -> Ok data
