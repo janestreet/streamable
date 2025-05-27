f43612641db8a36045aa00634703402049a32180
public-release <as-public-release@janestreet.com>
2025-05-27 19:09:59 +0100

    v0.18~preview.130.33+516

--- src/remove_t.ml
+++ src/remove_t.ml
@@ -4,12 +4,20 @@ open! Import
 (** Useful for removing the [type t] in the output of one of the streamable functors.
 
     Note: these functors will never change. It's fine to use them in stable contexts. *)
-module F (S : Module_type.S) = struct
-  module Streamable_impl : Module_type.S with type t := S.t = S
+module%template.portable [@modality p] F (S : Module_type.S) = struct
+  module Streamable_impl : sig
+    include Module_type.S with type t := S.t
+  end =
+    S
+
   include Streamable_impl
 end
 
-module F_rpc (S : Module_type.S_rpc) = struct
-  module Streamable_impl : Module_type.S_rpc with type t := S.t = S
+module%template.portable [@modality p] F_rpc (S : Module_type.S_rpc) = struct
+  module Streamable_impl : sig
+    include Module_type.S_rpc with type t := S.t
+  end =
+    S
+
   include Streamable_impl
 end
